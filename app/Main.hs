677c62ec3ce617a3848e39129946f2d1462d6e3f
LT Tsai <lt@ltt.pw>
2026-06-11 22:17:32 +0800

    project: add logger

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,25 @@ module Main where
 
 import RIO
 
+data Env = Env
+  { envLogFunc :: LogFunc
+  }
+
+instance HasLogFunc Env where
+  logFuncL = lens getter setter
+    where
+      getter env = env.envLogFunc
+      setter env lf = env {envLogFunc = lf}
+
+run :: RIO Env ()
+run = do
+  logInfo "Hello, ichiproxy"
+  logInfo "now running under my own Env"
+
 main :: IO ()
-main = runSimpleApp $ logInfo "Hello, ichiproxy"
+main = do
+  logOpts <-
+    logOptionsHandle stderr True
+  --  <&> setLogUseTime False
+  withLogFunc logOpts $ \lf ->
+    runRIO Env {envLogFunc = lf} run
