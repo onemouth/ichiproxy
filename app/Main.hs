8f21a8bee743dbc98f38549eb0cfcdbf523a9d98
LT Tsai <lt@ltt.pw>
2026-06-13 12:41:57 +0800

    connEnv: setup

--- app/Main.hs
+++ app/Main.hs
@@ -8,6 +8,12 @@ data Env = Env
     envListener :: NS.Socket
   }
 
+data ConnEnv = ConnEnv
+  { connEnvOuter :: Env,
+    connEnvConn :: NS.Socket,
+    connEnvPeer :: NS.SockAddr
+  }
+
 instance HasLogFunc Env where
   logFuncL = lens getter setter
     where
@@ -23,6 +29,33 @@ instance HasListener Env where
       getter env = env.envListener
       setter env s = env {envListener = s}
 
+outerL :: Lens' ConnEnv Env
+outerL = lens getter setter
+  where
+    getter ce = ce.connEnvOuter
+    setter ce e = ce {connEnvOuter = e}
+
+instance HasLogFunc ConnEnv where
+  logFuncL = outerL . logFuncL
+
+class HasConn env where
+  connL :: Lens' env NS.Socket
+
+instance HasConn ConnEnv where
+  connL = lens getter setter
+    where
+      getter ce = ce.connEnvConn
+      setter ce s = ce {connEnvConn = s}
+
+class HasPeer env where
+  peerL :: Lens' env NS.SockAddr
+
+instance HasPeer ConnEnv where
+  peerL = lens getter setter
+    where
+      getter ce = ce.connEnvPeer
+      setter ce p = ce {connEnvPeer = p}
+
 openListenerIO :: Int -> IO NS.Socket
 openListenerIO port = do
   sock <- NS.socket NS.AF_INET NS.Stream 0
@@ -32,17 +65,21 @@ openListenerIO port = do
   NS.listen sock 5
   pure sock
 
-handleConn :: (HasLogFunc env) => NS.Socket -> NS.SockAddr -> RIO env ()
-handleConn conn peer =
+handleConn :: (HasLogFunc env, HasConn env, HasPeer env) => RIO env ()
+handleConn = do
+  peer <- view peerL
+  conn <- view connL
   logInfo ("got connection from " <> displayShow peer)
     `finally` liftIO (NS.close conn)
 
-acceptLoop :: (HasLogFunc env, HasListener env) => RIO env ()
+acceptLoop :: RIO Env ()
 acceptLoop = do
+  outer <- ask
   sock <- view listenerL
   forever $ do
     (conn, peer) <- liftIO (NS.accept sock)
-    void $ async (handleConn conn peer)
+    let connEnv = ConnEnv outer conn peer
+    void $ async (runRIO connEnv handleConn)
 
 main :: IO ()
 main = do
